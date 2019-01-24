f6bfd011ba1a446fc10a46853d5eb2fa98fbc5c6
Alvaro Fernandez Garcia <afernandezgarcia@serbatic.es>
2019-01-24 14:07:15 +0100

    clean logs

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -2,28 +2,25 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import Footer from '../components/Footer';
 import { NOMBRE } from '../utils/constants';
-import '../style/main.css'
+import '../style/main.css';
 
 const SCREEN_HEIGHT = window.innerHeight;
 
-export default ({ children }) => {
-  console.log({ SCREEN_HEIGHT })
-  return (
-    <div style={{ height: SCREEN_HEIGHT }}>
-      <Helmet>
-        <title>{NOMBRE}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <link
-          rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-          integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
-          crossOrigin="anonymous"
-        />
-        <link rel="stylesheet" href="./style/main.css" />
-      </Helmet>
-      {/* <Header /> */}
-      {children}
-      <Footer />
-    </div>
-  );
-};
+export default ({ children }) => (
+  <div style={{ height: SCREEN_HEIGHT }}>
+    <Helmet>
+      <title>{NOMBRE}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <link
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+        integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+        crossOrigin="anonymous"
+      />
+      <link rel="stylesheet" href="./style/main.css" />
+    </Helmet>
+    {/* <Header /> */}
+    {children}
+    <Footer />
+  </div>
+);
