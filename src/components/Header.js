844b25d215eb43d96315c2f05064fb2a05f2f7f8
Alvaro Fernandez <alvarofg95dev@gmail.com>
2019-04-16 16:16:54 +0200

    hide header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default () => (
+export default () => null;
+/* export default () => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <a href="/">Inicio</a>
     <a href="/contacto">Contacto</a>
   </nav>
 );
+ */
