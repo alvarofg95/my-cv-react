e41647437a143592b25637505dfd66cf06ec80fd
Alvaro Fernandez <alvarofg95dev@gmail.com>
2019-04-16 16:17:35 +0200

    clean code

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import { Route } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import { NOMBRE } from '../utils/constants';
