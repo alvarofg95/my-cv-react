16bf45e08a2abc9fe16568076d3d20fa1407f1e2
Alvaro Fernandez <alvarofg95dev@gmail.com>
2019-04-16 16:15:08 +0200

    new footer version & linkedIn link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { CURRENT_VERSION } from '../utils/constants';
 
 export default ({ language }) => (
-  <div className="text-muted align-footer">
+  <div className="align-footer">
     <span>{CURRENT_VERSION}</span>
   </div>
 );
