d1b87fd0102a3feba7a870146e9e1deb0c7e00c2
Alvaro Fernandez <alvarofg95dev@gmail.com>
2019-04-24 10:18:38 +0200

    Pruebas Contacto

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,20 +2,20 @@ import React from 'react';
 import CustomButton from './CustomButton';
 
 export default () => (
-  <form name="contact" method="POST" data-netlify="true" netfily>
+  <form name="contact" method="POST" data-netlify="true" action="/contacto">
     <div className="display-inline-flex">
       <span className="spanForm">Nombre</span>
-      <input className="inputForm" type="text" />
+      <input className="inputForm" name="name" type="text" />
     </div>
     <br />
     <div className="display-inline-flex">
       <span className="spanForm">Email</span>
-      <input className="inputForm" type="email" />
+      <input className="inputForm" name="email" type="email" />
     </div>
     <br />
     <div className="display-inline-flex">
       <span className="spanForm">Mensaje</span>
-      <textarea className="inputForm textarea" type="text" />
+      <textarea className="inputForm textarea" name="message" type="text" />
     </div>
     <br />
     <CustomButton text="Enviar" type="submit" />
