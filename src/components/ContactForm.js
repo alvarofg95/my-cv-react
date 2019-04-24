428c4fcaeb623ffc8ad35fee9c02bffad847e902
Alvaro Fernandez <alvarofg95dev@gmail.com>
2019-04-24 10:51:51 +0200

    form render

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,32 +3,23 @@ import NetlifyForm from 'react-netlify-form';
 import CustomButton from './CustomButton';
 
 export default () => (
-  <NetlifyForm name="Contact Form">
-    {({ success }) => {
-      if (!success) {
-        return (
-          <div>
-            <div className="display-inline-flex">
-              <span className="spanForm">Nombre</span>
-              <input className="inputForm" name="name" type="text" />
-            </div>
-            <br />
-            <div className="display-inline-flex">
-              <span className="spanForm">Email</span>
-              <input className="inputForm" name="email" type="email" />
-            </div>
-            <br />
-            <div className="display-inline-flex">
-              <span className="spanForm">Mensaje</span>
-              <textarea className="inputForm textarea" name="message" type="text" />
-            </div>
-            <br />
-            <button type="submit">Enviar</button>
-          </div>
-        );
-      } else {
-        return <span>Gracias por contactar conmigo</span>;
-      }
-    }}
-  </NetlifyForm>
+  <form name="contact" method="post">
+    <input type="hidden" name="form-name" value="contact" />
+    <div className="display-inline-flex">
+      <span className="spanForm">Nombre</span>
+      <input className="inputForm" name="name" type="text" />
+    </div>
+    <br />
+    <div className="display-inline-flex">
+      <span className="spanForm">Email</span>
+      <input className="inputForm" name="email" type="email" />
+    </div>
+    <br />
+    <div className="display-inline-flex">
+      <span className="spanForm">Mensaje</span>
+      <textarea className="inputForm textarea" name="message" type="text" />
+    </div>
+    <br />
+    <button type="submit">Enviar</button>
+  </form>
 );
