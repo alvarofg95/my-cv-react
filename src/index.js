46a92c4d3731bb48f744fa2ea3a610550852efb0
Álvaro Fernández García <afernandezgarcia@serbatic.es>
2019-11-15 13:19:58 +0100

    polyfills and seo

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import WebFont from 'webfontloader';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
 import App from './App';
+import { setPolyfills } from './utils/utils';
 import * as serviceWorker from './serviceWorker';
 import './style/main.css';
 import './style/projects.css';
@@ -14,6 +15,8 @@ WebFont.load({
     families: ['Poppins']
   }
 });
+
+setPolyfills();
 
 ReactDOM.render(
   <I18nextProvider i18n={i18n}>
